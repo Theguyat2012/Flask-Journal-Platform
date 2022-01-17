7cdb3a9129d6e0fa3621cc6e67d492a0d7a0e172
Alex Ho <ho.alex100@gmail.com>
2022-01-17 17:44:38 -0500

    Add video cards to index

--- video_platform/main/routes.py
+++ video_platform/main/routes.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template
+from video_platform.models import Video
 
 main = Blueprint('main', __name__)
 
 
 @main.route("/")
 def index():
-    return render_template('main/index.html')
+    videos = Video.query.all()
+    return render_template('main/index.html', videos=videos)
