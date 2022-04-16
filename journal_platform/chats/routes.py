89614a77de79554716b24c56633bb3d46cc209d4
Alex Ho <ho.alex100@gmail.com>
2022-04-16 11:43:56 -0400

    Differentiate between message senders in frontend

--- journal_platform/chats/routes.py
+++ journal_platform/chats/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from flask_login import current_user
 from flask_socketio import send
 from journal_platform import socketio, db
@@ -8,29 +8,33 @@ chats = Blueprint('chats', __name__)
 
 
 @socketio.on('message')
-def handle_message(message):
-    send(message, broadcast=True)
+def handle_message(json):
+    send(json, broadcast=True)
 
 @socketio.on("send_message")
 def send_message(json):
-    print("hello")
-    print(json)
-    print(json['message'])
-    print(json['chat_id'])
-    message = Message(content=json['message'], user_id=current_user.id, chat_id=json['chat_id'])
+    message = Message(content=json['message'], user_id=json['user_id'], chat_id=json['chat_id'])
     db.session.add(message)
     db.session.commit()
 
 @chats.route('/chat')
-def chat_list():
-    chat_list = Chat.query.filter(Chat.chat_users.any(id=current_user.id))
-    return render_template('chats/chat_list.html', chat_list=chat_list)
+def list():
+    # FIXME: Rework the list of chats
+    if current_user.is_authenticated:
+        chat_list = Chat.query.filter(Chat.chat_users.any(id=current_user.id))
+        return render_template('chats/list.html', chat_list=chat_list)
+
+    return redirect(url_for('main.index'))
 
 @chats.route('/chat/<int:chat_id>')
 def chat(chat_id):
-    chat = Chat.query.filter_by(id=chat_id).first()
-    messages = chat.messages
-    chat_users = chat.chat_users
+    # FIXME: Rework the individual chat page
+    if current_user.is_authenticated:
+        chat = Chat.query.filter_by(id=chat_id).first()
+        if chat is not None and current_user in chat.chat_users:
+            messages = chat.messages
+            chat_users = chat.chat_users
+            return render_template('chats/chat.html', chat=chat, messages=messages, chat_users=chat_users)
+        return redirect(url_for('chats.list'))
 
-    return render_template('chats/chat.html', chat=chat, messages=messages, chat_users=chat_users)
-    # return render_template('chats/chat.html')
+    return redirect(url_for('main.index'))
